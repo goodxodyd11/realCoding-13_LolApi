f584261fbb1664a13d9cee5761608f8982de9e54
JTY <wjdxodyd11@gmail.com>
2019-04-30 16:52:12 +0900

    initalize project

---
+++ src/main/java/org/cnu/realcoding/lolapi/team13/lolapiteam13/Lolapiteam13Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.lolapi.team13.lolapiteam13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lolapiteam13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lolapiteam13Application.class, args);
+    }
+
+}
