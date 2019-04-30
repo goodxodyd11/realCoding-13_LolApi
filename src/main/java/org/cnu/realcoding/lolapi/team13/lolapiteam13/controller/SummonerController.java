d4183df660decee731ac8dfd90116f5afa69dc87
JTY <wjdxodyd11@gmail.com>
2019-04-30 18:11:01 +0900

    add SummonerMapApi

---
+++ src/main/java/org/cnu/realcoding/lolapi/team13/lolapiteam13/controller/SummonerController.java
@@ -0,0 +1,10 @@
+package org.cnu.realcoding.lolapi.team13.lolapiteam13.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SummonerController {
+
+
+}
